422a7eb166245a6295cd7d420eb9841b6d531740
Alaa Azazi <alaa.azazi@vizworx.com>
2017-08-02 11:08:16 -0600

    Added a screen resize handler.

--- app/app.jsx
+++ app/app.jsx
@@ -4,13 +4,17 @@ const ReactRedux = require('react-redux')
 const React = require('react')
 
 const Root = require('./components/Root.jsx')
-const Resized = require('./actionCreators/ResizeScreenCreator')
-const store = require('./Store')()
+const Resized = require('./actionCreators/ResizeScreenCreator.js')
+const store = require('./Store.js')()
 
 window.store = store
 window.resized = Resized
 
 DomReady( () => {
+
+  resizeScreenHandler()
+  window.addEventListener('resize', resizeScreenHandler)
+
   const app = <ReactRedux.Provider store={store}>
     <Root />
   </ReactRedux.Provider>
@@ -18,3 +22,10 @@ DomReady( () => {
   ReactDOM.render(app, document.getElementById('reactRoot'))
 })
 
+function resizeScreenHandler()  
+{
+  	var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+	store.dispatch(Resized(w,h))
+}
