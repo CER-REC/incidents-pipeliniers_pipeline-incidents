c3764923a16dc8d76cc576e34bc9d0a0ccd57910
ccheuvizworx <30633884+ccheuvizworx@users.noreply.github.com>
2017-08-02 17:13:14 -0600

    added resized house to top bar

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,22 +1,28 @@
 const React = require('react')
 const ReactRedux = require('react-redux')
+const Constants = require('../Constants.js')
 
 //home image
 
 
 //home button
-const homeButton = () => (
-		<button>
-			<home />
-			<span>home</span>
-		</button>
-	)
+function homeButton() {
+			console.log(Constants.get('topBar').get('homeIconWidth'))
+			console.log(Constants.getIn(['topBar', 'homeIconWidth']))
+
+			let image = <image xlinkHref='images/home.svg' 
+				height = {Constants.getIn(['topBar', 'homeIconHeight'])}
+				width = {Constants.getIn(['topBar', 'homeIconWidth'])}
+			></image>
+			return image
+}
 
 //heading
 class Header extends React.Component {
 	render() {
-		return (<g>
-				<image xlinkHref='images/home.svg'></image>
+		let transformString = `translate(${Constants.get('leftOuterMargin')},${Constants.get('topOuterMargin')})`
+		return (<g transform = {transformString}>
+				{homeButton()}
 				<text></text>
 				<text></text>
 				<text></text>
