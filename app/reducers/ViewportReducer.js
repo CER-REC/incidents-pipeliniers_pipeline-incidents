1aab2abe21f959df9694ef943fbd3fa22d0d1702
Alaa Azazi <alaa.azazi@vizworx.com>
2017-08-03 12:34:35 -0600

    ViewportReducer ResizeScreen validation.

--- app/reducers/ViewportReducer.js
+++ app/reducers/ViewportReducer.js
@@ -1,5 +1,7 @@
 const Immutable = require('immutable')
 
+const Constants = require('../Constants.js')
+
 const defaults = Immutable.fromJS({
   x: 0,
   y: 0
@@ -10,13 +12,17 @@ const ViewportReducer = (state = defaults, action) => {
   switch(action.type) {
 
   case 'ResizeScreen':
-    // TODO: validate this
-    // numbers
-    // max width 1200
-    
+    // Check that the action is a valid object.
+    if (action && action.x && action.y)
+      // Check that the dimensions are valid.
+      // TODO: Increase the height of the workspace by emptyCategoryOffsetRatio if
+      // the empty categories are visible (i.e. empty categories state is visible).
+      if (action.x > Constants.getIn(['workspace', 'maxWidth'])) 
+        action.x = Math.min(Constants.getIn(['workspace', 'maxWidth']), action.x)
+        action.y = action.x * Constants.getIn(['workspace', 'heightToWidthRatio'])
+      return state.set('x', action.x).set('y', action.y)
 
-    return state.set('x', action.x).set('y', action.y)
-
+    return state
   default:
     return state
   }
