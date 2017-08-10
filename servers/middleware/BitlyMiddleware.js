10445f4d92e41fa17c8dd2d89e44291601218ffa
ccheuvizworx <30633884+ccheuvizworx@users.noreply.github.com>
2017-08-10 11:20:55 -0600

    middleware

---
+++ servers/middleware/BitlyMiddleware.js
@@ -0,0 +1,28 @@
+
+
+const express = require('express')
+
+//mock endpoint - designed to resemble a bitly request but doesn't actually do so
+function handler(request, res) {
+  res.setHeader('content-type', 'application/json')
+  res.write(JSON.stringify( {
+    status_code: 200,
+    status_txt: 'OK',
+    data:{
+      url: 'https://apps2.neb-one.gc.ca/incident-visualization',
+      hash: 'asdfdsfkl',
+      global_hash: 'asdcxb',
+      long_url: 'https://apps2.neb-one.gc.ca/incident-visualization', 
+      new_hash: 0
+    }
+  }))
+  res.end()
+}
+
+const BitlyMiddleware = function() {
+  const app = express()
+  app.get('/bitly_url', handler)
+  return app
+}
+
+module.exports = BitlyMiddleware
