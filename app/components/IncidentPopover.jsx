51427b8fd826f52f4cef0ba747a5ca05c81320fa
ccheuvizworx <30633884+ccheuvizworx@users.noreply.github.com>
2017-08-16 10:02:43 -0600

    popover visual elements

--- app/components/IncidentPopover.jsx
+++ app/components/IncidentPopover.jsx
@@ -1,42 +1,50 @@
 const React = require('react')
 const ReactRedux = require('react-redux')
 
-//probably import incidentBar
 const IncidentBar = require('./IncidentBar.jsx')
 const Constants = require('../Constants.js')
 
 class IncidentPopover extends React.Component {
-  render() {
-    const height = Constants.getIn(['incidentPopover', 'height'])
-    const width = Constants.getIn(['incidentPopover', 'width'])
-    let transformPopover = `translate(${Constants.getIn(['pinColumn','TODO'])},${Constants.getIn(['pinColumn','TODO'])})`
-    return <g>
 
-      <text x={0} y={150}
-        //transform = {transformPopover}
-        height = {height}
-        width = {width}
+  showPinImage() {
+    const height = Constants.getIn(['pinColumn', 'pinIconSize'])
+    const width = Constants.getIn(['pinColumn', 'pinIconSize'])
+    return <image 
+      height = {height} 
+      width = {width} 
+      x="-15" y="-15" //change to constants once sure
+      xlinkHref='images/unpinned.svg'></image>
+  }
+
+  showBorder() {
+    return <svg 
+      x="1" y="-8" //change to constants
+      xmlnsXlink='http://www.w3.org/1999/xlink'> //svg height and width bigger
+      <line x1={0} y1={0} x2={78} y2={0} stroke="#888889" stroke-width="1" /> //horizontal line
+      <line x1={78} y1={0} x2={78} y2={55} stroke="#888889" stroke-width="1" /> //vertical line
+      <line x1={78} y1={55} x2={83} y2={55} stroke="#888889" stroke-width="1" /> //horizontal stub
+      <circle cx="85" cy="55" r="3" fill="#888889"/>
+    </svg>
+  }
+
+  render() {
+    const transformPopover = 'translate(65,170)' //dependent on location of incident in the bar?
+    return <g transform = {transformPopover}>
+      {this.showPinImage()}
+      {this.showBorder()}
+      <text
         className="subpop">
-        <tspan x="0" dy=".6em">INCIDENT NUMBER</tspan>
-        <tspan x="0" dy="1.2em">near LOCATION</tspan>
-        <tspan x="0" dy="1.2em">date reported:</tspan>
-        <tspan x="0" dy="1.2em">DATE</tspan>
+        <tspan x={0} dy=".6em">INCIDENT#</tspan>
+        <tspan x={0} dy="1.2em">Near LOCATION</tspan>
+        <tspan x={0} dy="1.2em">Date reported:</tspan>
+        <tspan x={0} dy="1.2em">DATE</tspan>
       </text>
-      //pin icon
-      //top bar thing
-      //connector to the column (bar and ball/dot thing)
     </g>
-    //1. transform element for the group
   }
   /**
     TODO: Should show popover when selected
     cursor should change to pointer
-    popover needs to use dimensions for height and width given in Constants.js
     pinned and unpinned image needs to update depending on selection
-      draws a line over the top 78x50 1px stroke
-      width +5px for 'L' shape
-      grey 6x6 circle from line to incidentBar
-    popover colour : #888889
     popover should disappear when user clicks away
   **/
 
