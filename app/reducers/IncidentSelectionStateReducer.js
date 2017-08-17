c45869ae5ec35111fe546bd6b2f973bcdc12b47f
Patrick King <patrick.king@vizworx.com>
2017-08-17 16:03:34 -0600

    Handle de-selecting the incident when it becomes pinned.

--- app/reducers/IncidentSelectionStateReducer.js
+++ app/reducers/IncidentSelectionStateReducer.js
@@ -8,6 +8,13 @@ const IncidentSelectionStateReducer = (state = null, action) => {
   case 'IncidentDeselectionState':
     return null
 
+  case 'AddPinnedIncident':
+    // Some assumptions:
+    // - An incident can either be selected, or pinned, but not both. 
+    // - Only the currently selected incident can become pinned
+    // So, when we pin an incident we should clear the selection.
+    return null
+
   default:
     return state
   }
