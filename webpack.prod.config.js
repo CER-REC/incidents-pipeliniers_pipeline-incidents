f397882d7199e3f4f47ecd520fe01a735a42e4ea
Patrick King <patrick.king@vizworx.com>
2017-08-29 16:29:22 -0600

    Try to make a production config.js

---
+++ webpack.prod.config.js
@@ -0,0 +1,18 @@
+const Webpack = require('webpack')
+
+const config = require('./webpack.config.js')
+
+const prodPlugin = new Webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production')
+  }
+})
+
+// TODO: do I need this?
+const uglifyPlugin = new Webpack.optimize.UglifyJsPlugin()
+
+config.plugins.push(prodPlugin)
+config.plugins.push(uglifyPlugin)
+
+module.exports = config
+
