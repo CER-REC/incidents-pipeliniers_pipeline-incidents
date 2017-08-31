3af466ac06039be23f2fe30653096b73b0f1260c
Alaa Azazi <alaa.azazi@vizworx.com>
2017-08-30 23:31:00 -0600

    DragSidebarColumnReducer sets individual action members rather than merging into state.

--- app/reducers/SidebarColumnDragReducer.js
+++ app/reducers/SidebarColumnDragReducer.js
@@ -12,13 +12,17 @@ const SidebarColumnDragReducer = (state = defaults, action) => {
   switch(action.type) {
 
   case 'DragSidebarColumn':
-    return state.merge(action)
+    return state.set('newX', action.newX)
 
   case 'DragSidebarColumnStarted':
-    return state.merge(action)
+    return state.set('isStarted', action.isStarted)
+      .set('columnName', action.columnName)
+      .set('oldX', action.oldX)
+      .set('newX', action.newX)
+      .set('offset', action.offset)
 
   case 'DragSidebarColumnEnded':
-    return state.merge(action)
+    return state.set('isStarted', action.isStarted)
 
   default:
     return state
