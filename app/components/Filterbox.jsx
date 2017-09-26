c3906aab264af9e8ff249e361de1ca4faff41f2e
ccheuvizworx <30633884+ccheuvizworx@users.noreply.github.com>
2017-09-26 14:21:41 -0600

    added language logic

--- app/components/Filterbox.jsx
+++ app/components/Filterbox.jsx
@@ -81,26 +81,49 @@ class Filterbox extends React.Component {
 
 
   dragButton() {
-    return <g className='filterBoxButton'>
-      <rect
-        className='filterBoxRect'
-        x = { Constants.getIn(['filterbox', 'filterButtonWidth']) }
-        y = '0'
-        width = { Constants.getIn(['filterbox', 'dragButtonWidth']) }
-        height = { this.buttonHeight() }
-      />
-      <image 
-        xlinkHref='images/vertical_drag.svg' 
-        className = 'verticalDrag'
-        x = { Constants.getIn(['filterbox', 'filterButtonWidth']) + Constants.getIn(['filterbox', 'dragIconHorizontalOffset']) }
-        y = '0'
-        width = { Constants.getIn(['filterbox', 'dragIconWidth']) }
-        height = { this.buttonHeight() }
-        onMouseDown={this.handleDragStart.bind(this)}
-        onMouseMove={this.handleDragMove.bind(this)}
-        onMouseUp={this.handleDragEnd.bind(this)}
-      />
-    </g> 
+    if(this.props.language === 'en') {
+      return <g className='filterBoxButton'>
+        <rect
+          className='filterBoxRect'
+          x = { Constants.getIn(['filterbox', 'filterButtonWidth']) }
+          y = '0'
+          width = { Constants.getIn(['filterbox', 'dragButtonWidth']) }
+          height = { this.buttonHeight() }
+        />
+        <image 
+          xlinkHref='images/vertical_drag.svg' 
+          className = 'verticalDrag'
+          x = { Constants.getIn(['filterbox', 'filterButtonWidth']) + Constants.getIn(['filterbox', 'dragIconHorizontalOffset']) }
+          y = '0'
+          width = { Constants.getIn(['filterbox', 'dragIconWidth']) }
+          height = { this.buttonHeight() }
+          onMouseDown={this.handleDragStart.bind(this)}
+          onMouseMove={this.handleDragMove.bind(this)}
+          onMouseUp={this.handleDragEnd.bind(this)}
+        />
+      </g> 
+    } else if(this.props.language === 'fr') {
+      return <g className='filterBoxButton'>
+        <rect
+          className='filterBoxRect'
+          x = {100}
+          y = '0'
+          width = { Constants.getIn(['filterbox', 'dragButtonWidth']) }
+          height = { this.buttonHeight() }
+        />
+        <image 
+          xlinkHref='images/vertical_drag.svg' 
+          className = 'verticalDrag'
+          x = { 100 + Constants.getIn(['filterbox', 'dragIconHorizontalOffset']) }
+          y = '0'
+          width = { Constants.getIn(['filterbox', 'dragIconWidth']) }
+          height = { this.buttonHeight() }
+          onMouseDown={this.handleDragStart.bind(this)}
+          onMouseMove={this.handleDragMove.bind(this)}
+          onMouseUp={this.handleDragEnd.bind(this)}
+        />
+      </g> 
+    }
   }
 
   handleDragStart(e) {
