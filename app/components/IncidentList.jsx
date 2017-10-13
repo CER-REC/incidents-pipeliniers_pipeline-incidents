276bb183b6a908767b670f6351669236e0fc3662
Patrick King <patrick.king@vizworx.com>
2017-10-12 21:17:40 -0600

    Don't show the blank list message when there are starred incidents.

--- app/components/IncidentList.jsx
+++ app/components/IncidentList.jsx
@@ -29,14 +29,14 @@ class IncidentList extends React.Component {
   // Render Helpers
 
   noIncidentsText() {
-    if (this.props.filterboxActivationState.get('columnName') === null) {
-      return <div className='noIncidentsTextBlock'>
-        <p className = 'noIncidentsText'>{ Tr.getIn(['noCategorySelection', this.props.language])}</p>
-      </div>
-    }
-    else {
+    if (this.props.filterboxActivationState.get('columnName') !== null ||
+      this.props.pinnedIncidents.count() > 0) {
       return null
     }
+
+    return <div className='noIncidentsTextBlock'>
+      <p className = 'noIncidentsText'>{ Tr.getIn(['noCategorySelection', this.props.language])}</p>
+    </div>
   }
 
   incidentList() {
