7a1b42e29a8fe3299ff81b6677b1e8bda8b930ec
Adam Lovatt <adam.lovatt@vizworx.com>
2019-09-17 11:53:17 -0600

    Removes pre-build audit because we're so out of date it will always fail the build

--- build.js
+++ build.js
@@ -3,10 +3,6 @@ const PackageJson = require('./package.json')
 const Fs = require('fs-extra')
 const Path = require('path')
 
-
-console.log('Node Security Project known vulnerabilities check:')
-ExecSync('nsp check', {stdio: [0,1,2]})
-
 // Build the javascript bundle for production
 ExecSync('npm run webpack')
 
@@ -14,7 +10,7 @@ Fs.writeFile(Path.join(__dirname, 'VERSION'), PackageJson.version, function(err)
   if (err) {
     throw err
   }
-}); 
+});
 
 const git_last_modified_date = ExecSync('git show -s --format=%cd HEAD --date=short')
 
@@ -22,7 +18,7 @@ Fs.writeFile(Path.join(__dirname, 'LAST_MODIFIED'), git_last_modified_date, func
   if (err) {
     throw err
   }
-}); 
+});
 
 
 console.log('Build done!');
